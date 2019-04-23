70440c862d1bdb642d13465893c65bbca6596e75
Yoshiaki Ito <ito@maboroshi.biz>
2019-04-23 18:50:09 +0900

    画像を動的に表示切替するコンポーネントをCompositeReel.vueにて追加した

---
+++ src/components/composites/CompositeReel.vue
@@ -0,0 +1,34 @@
+<template>
+  <VImg :src="path" @click="imgClickHandler" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class CompositeReel extends Vue {
+  /**
+   * 内部ステートを定義する
+   */
+  @Prop({ required: true }) imageList: string[] = []
+
+  idx: number = 0
+  cnt: number = 0
+
+  /**
+   * 算出プロパティを定義する
+   */
+  get path() {
+    return this.imageList[this.idx]
+  }
+
+  /**
+   * イベントハンドラを定義する
+   */
+  imgClickHandler() {}
+}
+</script>
+
+<style scoped lang="stylus"></style>
