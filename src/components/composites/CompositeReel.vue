9029ae8212ad23937a93a9ef9b71ec400805b5c9
oti <otiext@gmail.com>
2019-04-28 00:31:16 +0900

    リールをクリックして画像をランダムに表示し再びクリックで止める処理を追加した

--- src/components/composites/CompositeReel.vue
+++ src/components/composites/CompositeReel.vue
@@ -1,5 +1,12 @@
 <template>
-  <VImg :src="path" @click="imgClickHandler" />
+  <div class="Reel">
+    <div
+      class="Reel__image"
+      :style="{ 'background-image': 'url(' + path + ')' }"
+      @click="reelClickHandler"
+    />
+    <p class="Reel__count mb-0">{{ count }}</p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,21 +21,68 @@ export default class CompositeReel extends Vue {
    */
   @Prop({ required: true }) imageList!: string[]
 
+  /**
+   * 内部ステートを定義する
+   */
+  // ループインターバル
+  interval: any
+  // リールする画像の数
+  length: number = this.imageList.length
+  // 画像配列の添字
   idx: number = 0
-  cnt: number = 0
+  // リールの状態
+  status: 'ready'|'rolling'|'stopped' = 'ready'
+
+  // 0〜imageList.lengthの間のランダムな整数を返す
+  random() {
+    return Math.floor(Math.random() * this.length)
+  }
+
+  // リールを回す
+  startReel() {
+    this.status = 'rolling'
+    this.interval = setInterval(() => {
+      this.idx = this.random()
+    }, 10)
+  }
+
+  stopReel() {
+    this.status = 'stopped'
+    clearInterval(this.interval)
+  }
 
   /**
    * 算出プロパティを定義する
    */
+
+  // 画像パスを返す
   get path() {
     return this.imageList[this.idx]
   }
 
-  /**
+/**
    * イベントハンドラを定義する
    */
-  imgClickHandler() {}
+  reelClickHandler() {
+    if (this.status === 'rolling') {
+      this.stopReel()
+    } else {
+      this.startReel()
+    }
+  }
 }
 </script>
 
-<style scoped lang="stylus"></style>
+<style scoped lang="stylus">
+.Reel__image
+  cursor pointer
+  padding-top 100%
+  border 2px solid #000
+  width 100%
+  background-position center center
+  background-color gold
+  background-size 100% auto
+
+.Reel__count
+  text-align center
+</style>
