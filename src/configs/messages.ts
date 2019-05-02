1253509b4b9a16e5c92f785f98c6ff1d2bbf1bb4
oti <otiext@gmail.com>
2019-05-03 01:42:04 +0900

    メッセージテキストの定数を追加した

--- src/configs/messages.ts
+++ src/configs/messages.ts
@@ -1,3 +1,17 @@
 export const INVALID_CODE = 'コードが正しくありません'
 export const NO_DATA = 'データがありません'
 export const REQUEST_ERROR = 'リクエストエラー'
+export const ROLLING = 'そろうかな〜？'
+export const WIN = [
+  'うわぁそろってる……すごいね……',
+  '今日はかなりついてるね！',
+  '乙。',
+  'ﾔｯﾀｰ！ さぁTKGを食べよう！'
+]
+export const LOSE = [
+  'はい残念〜',
+  'おしい！',
+  'ﾝﾝｰwww',
+  'まだそろえられないの？',
+  'そろうまでやろうね！'
+]
