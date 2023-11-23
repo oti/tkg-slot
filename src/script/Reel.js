aa21586225b7fa67b59819c11732fc7c9b882f64
oti <otiext@gmail.com>
2023-11-23 16:57:18 +0900

    fix: src/script/Reel.js, `this.items` の数とシャッフルのインターバルを元に戻した

--- src/script/Reel.js
+++ src/script/Reel.js
@@ -3,7 +3,7 @@ export class Reel {
     this.id = 0;
     this.intervalId = null;
     this.isShuffling = false;
-    this.items = Array.from({ length: 3 }, (_, index) => index + 1);
+    this.items = Array.from({ length: 120 }, (_, index) => index + 1);
     this.$Reel = $Reel;
     this.$Pict = this.$Reel.querySelector(".Pict");
     this.$Stop = this.$Reel.querySelector(".Button");
@@ -29,7 +29,7 @@ export class Reel {
       this.id = this.items[Math.floor(Math.random() * this.items.length)];
       this.$Text.textContent = this.id;
       this.$Pict.classList.replace(old, this.classString);
-    }, 1000);
+    }, 1);
   }
 
   stop() {
